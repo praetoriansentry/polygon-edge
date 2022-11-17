be211dbe8cf4b475761ca47fc862cf0fb739198f
Goran Rojovic <goran.rojovic@ethernal.tech>
2022-11-17 14:49:07 +0100

    Bug fix

--- consensus/polybft/consensus_runtime_test.go
+++ consensus/polybft/consensus_runtime_test.go
@@ -1334,7 +1334,7 @@ func TestConsensusRuntime_calculateUptime_SecondEpoch(t *testing.T) {
 	blockchainMock.On("GetHeaderByNumber", mock.Anything).Return(headerMap.getHeader)
 
 	polybftBackendMock := new(polybftBackendMock)
-	polybftBackendMock.On("GetValidators", mock.Anything, mock.Anything).Return(validators.getPublicIdentities()).Once()
+	polybftBackendMock.On("GetValidators", mock.Anything, mock.Anything).Return(validators.getPublicIdentities()).Twice()
 
 	consensusRuntime := &consensusRuntime{
 		config: &runtimeConfig{
