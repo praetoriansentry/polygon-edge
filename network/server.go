5c918cfd6bda4327a9d2411f7454e5027a7dfecd
John Hilliard <praetoriansentry@gmail.com>
2022-12-07 18:32:26 -0500

    fix: trying larger buffers

--- network/server.go
+++ network/server.go
@@ -32,12 +32,12 @@ const (
 	// peerOutboundBufferSize is the size of outbound messages to a peer buffers in go-libp2p-pubsub
 	// we should have enough capacity of the queue
 	// because we start dropping messages to a peer if the outbound queue is full
-	peerOutboundBufferSize = 1024
+	peerOutboundBufferSize = 1024 * 1024
 
 	// validateBufferSize is the size of validate buffers in go-libp2p-pubsub
 	// we should have enough capacity of the queue
 	// because when queue is full, validation is throttled and new messages are dropped.
-	validateBufferSize = 1024
+	validateBufferSize = 1024 * 1024
 )
 
 const (
