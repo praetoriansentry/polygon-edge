cd3234665f16a6bd08987205b0072f9200a40efd
John Hilliard <praetoriansentry@gmail.com>
2022-12-07 17:53:52 -0500

    feat: enabling pprof

--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	_ "embed"
-
 	"github.com/0xPolygon/polygon-edge/command/root"
 	"github.com/0xPolygon/polygon-edge/licenses"
+	"net/http"
+	_ "net/http/pprof"
 )
 
 var (
@@ -14,6 +15,8 @@ var (
 
 func main() {
 	licenses.SetLicense(license)
-
+	go func() {
+		http.ListenAndServe("localhost:6060", nil)
+	}()
 	root.NewRootCommand().Execute()
 }
